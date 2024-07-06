7533364b8cecff192c4819bfa18d34fd4763be4b
Styop <stepan00999@gmail.com>
2024-07-06 22:41:29 +0400

    flights by time range

--- models/api/flights_by_operator.py
+++ models/api/flights_by_operator.py
@@ -1,19 +1,51 @@
 from ..models import Flights
-from datetime import datetime
-from django.http import JsonResponse,HttpResponse
+from datetime import datetime, time
+from django.http import JsonResponse, HttpResponse
 from ..tools import categorize_flights
 
 
 def get_flights_by_airoperator(request):
     air_operator = request.GET.get('operator')
+    start_time = request.GET.get('start_time')
+    end_time = request.GET.get('end_time')
+
+    current_datetime = datetime.now()
     if not air_operator:
         return HttpResponse(status=404)
-    future_flights = Flights.objects.filter(airoperator_name=air_operator,
-                                            departure_1_date_time__gt=datetime.now()).order_by('departure_1_date_time')
-    past_flights = Flights.objects.filter(airoperator_name=air_operator,
-                                          departure_1_date_time__lte=datetime.now()).order_by('departure_1_date_time')
+
+    if start_time:
+        start_time_obj = time(hour=int(start_time.split(':')[0]), minute=int(start_time.split(':')[1]))
+    else:
+        start_time_obj = None
+
+    if end_time:
+        end_time_obj = time(hour=int(end_time.split(':')[0]), minute=int(end_time.split(':')[1]))
+    else:
+        end_time_obj = None
+
+    if start_time_obj and end_time_obj:
+        future_flights = Flights.objects.filter(airoperator_name=air_operator,
+                                                departure_1_date_time__time__gte=start_time_obj,
+                                                departure_1_date_time__time__lte=end_time_obj,
+                                                departure_1_date_time__gt=current_datetime).order_by(
+            'departure_1_date_time')
+
+        past_flights = Flights.objects.filter(airoperator_name=air_operator,
+                                              departure_1_date_time__time__gte=start_time_obj,
+                                              departure_1_date_time__time__lte=end_time_obj,
+                                              departure_1_date_time__lte=current_datetime).order_by(
+            'departure_1_date_time')
+
+
+    else:
+        future_flights = Flights.objects.filter(airoperator_name=air_operator,
+                                                departure_1_date_time__gt=current_datetime).order_by(
+            'departure_1_date_time')
+
+        past_flights = Flights.objects.filter(airoperator_name=air_operator,
+                                              departure_1_date_time__lte=current_datetime).order_by(
+            'departure_1_date_time')
     data = {}
     data['future'] = categorize_flights(future_flights)
     data['past'] = categorize_flights(past_flights)
-
     return JsonResponse(data)
--- models/api/permission_number.py
+++ models/api/permission_number.py
@@ -1,21 +1,25 @@
-from datetime import datetime
+from datetime import datetime, time
 from django.shortcuts import render
 from ..models import Flights
 from ..tools import filter_permission_numbers
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse
+
 
 def get_permission_numbers(request):
-    start_date=request.GET.get('start_date')
+    start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
-    dt_start_date = None
-    dt_end_date = None
+    dt_start_date_with_time = None
+    dt_end_date_with_time = None
     try:
         dt_start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
         dt_end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+        dt_start_date_with_time = datetime.combine(dt_start_date, time.min)
+        dt_end_date_with_time = datetime.combine(dt_end_date, time.max)
     except Exception as e:
         pass
-    if dt_start_date and dt_end_date:
-        permission_numbers = Flights.objects.filter(sign_date__gte=dt_start_date,sign_date__lte=dt_end_date)
-        permission_number_dict =filter_permission_numbers(permission_numbers)
+    if dt_start_date_with_time and dt_end_date_with_time:
+        permission_numbers = Flights.objects.filter(sign_date__gte=dt_start_date_with_time,
+                                                    sign_date__lte=dt_end_date_with_time)
+        permission_number_dict = filter_permission_numbers(permission_numbers)
         return JsonResponse(permission_number_dict)
-    return render(request,'permissions_page.html')
+    return render(request, 'permissions_page.html')
